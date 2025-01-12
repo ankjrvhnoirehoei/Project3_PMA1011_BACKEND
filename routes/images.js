bf607d550c5359f9473428c3911a7d573aac8008
ankjrvhnoirehoei <166099409+ankjrvhnoirehoei@users.noreply.github.com>
2025-01-12 13:02:37 +0700

    fix database

--- routes/images.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var imageModel = require("../models/image-model");
-const JWT = require('jsonwebtoken');
-const config = require("../util/config");
-
-module.exports = router;
