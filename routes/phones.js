891ce57bbed028f2b76ab565264b53093168bafe
TrinhChanPhuc <trinhchanphuc123@gmail.com>
2025-02-15 07:04:19 +0700

    fixing onePhone API 06

--- routes/phones.js
+++ routes/phones.js
@@ -324,7 +324,7 @@ router.get("/onePhone", async function(req, res, next){
               }else{  // main activity goes here
                   try {
                       const { phoneID } = req.query;
-                      var onePhone = await phoneModel.find({phoneID : phoneID});
+                      var onePhone = await phoneModel.findOne({phoneID : phoneID});
                       res.status(200).json(onePhone);
                   } catch (error) {
                       res.json({status: false, message: "an error has occured"});
